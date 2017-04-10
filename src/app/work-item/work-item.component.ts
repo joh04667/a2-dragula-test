e880c39d05ca03df94657d31fc4692ccee0db150
Kyle Johnson <joh04667@gmail.com>
2017-04-09 21:07:57 -0500

    bags are not what i thought they were

--- src/app/work-item/work-item.component.ts
+++ src/app/work-item/work-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-work-item',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkItemComponent implements OnInit {
 
+  @Input() title;
+  @Input() message;
+  
   constructor() { }
 
   ngOnInit() {
